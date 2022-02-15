bc843f7d8114cb58a6be6ca2e23a34bf4abc87bc
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2022-02-15 10:15:31 +0100

    index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #CRUD 
+  # Read (all restaurants)
+  # Read (one restaurant)
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+
 end
